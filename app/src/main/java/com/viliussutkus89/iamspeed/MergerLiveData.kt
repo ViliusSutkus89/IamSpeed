174c3cf061b93a7d79f9aa832d7d46bd298f7faf
Vilius Sutkus '89 <ViliusSutkus89@gmail.com>
2022-09-12 04:20:39 +0300

    MergerLiveData.Two, because it's unused

--- app/src/main/java/com/viliussutkus89/iamspeed/MergerLiveData.kt
+++ app/src/main/java/com/viliussutkus89/iamspeed/MergerLiveData.kt
@@ -7,50 +7,6 @@ import androidx.lifecycle.MediatorLiveData
 // https://medium.com/nerd-for-tech/merging-livedata-like-you-need-it-3abcf6b756ca
 
 sealed class MergerLiveData<TargetType> : MediatorLiveData<TargetType>() {
-    class Two<FirstSourceType, SecondSourceType, TargetType>(
-        private val firstSource: LiveData<FirstSourceType>,
-        private val secondSource: LiveData<SecondSourceType>,
-        private val distinctUntilChanged: Boolean = true,
-        private val merging: (FirstSourceType, SecondSourceType) -> TargetType
-    ) : MediatorLiveData<TargetType>() {
-        override fun onActive() {
-            super.onActive()
-
-            addSource(firstSource) { value ->
-                val newValue =
-                    merging(
-                        value,
-                        secondSource.value ?: return@addSource
-                    )
-
-                postValue(
-                    distinctUntilChanged = distinctUntilChanged,
-                    newValue = newValue
-                )
-            }
-
-            addSource(secondSource) { value ->
-                val newValue =
-                    merging(
-                        firstSource.value ?: return@addSource,
-                        value
-                    )
-
-                postValue(
-                    distinctUntilChanged = distinctUntilChanged,
-                    newValue = newValue
-                )
-            }
-        }
-
-        override fun onInactive() {
-            removeSource(firstSource)
-            removeSource(secondSource)
-
-            super.onInactive()
-        }
-    }
-
     class Three<FirstSourceType, SecondSourceType, ThirdSourceType, TargetType>(
         private val firstSource: LiveData<FirstSourceType>,
         private val secondSource: LiveData<SecondSourceType>,
